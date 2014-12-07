86b442bc47dcd1ceb30edbfec9b123e6c559723b
Mohandas Nur Alfan <alfanndaz@gmail.com>
2014-12-07 21:32:16 +0700

    alfan: del coment cont menus

--- app/controllers/menus.java
+++ app/controllers/menus.java
@@ -11,28 +11,6 @@ import play.exceptions.TemplateNotFoundException;
 import controllers.CRUD.ObjectType;
 
 public class menus extends CRUD {
-	/*
-	public static void list(int page, String search, String searchFields, String orderBy, String order) {
-        ObjectType type = ObjectType.get(getControllerClass());
-        notFoundIfNull(type);
-        if (page < 1) {
-            page = 1;
-        }
-        List<Model> objects = type.findPage(page, search, searchFields, orderBy, order, (String) request.args.get("where"));
-        Long count = type.count(search, searchFields, (String) request.args.get("where"));
-        Long totalCount = type.count(null, null, (String) request.args.get("where"));
-        List<menu> rr = menu.findAll();
-        for(menu nn: rr){
-        	nn.Harga = nn.Nama_Resep.Harga_menu ;
-        	nn.save();
-        }
-        try {
-            render(type, objects, count, totalCount, page, orderBy, order);
-        } catch (TemplateNotFoundException e) {
-            render("CRUD/list.html", type, objects, count, totalCount, page, orderBy, order);
-        }
-    }
-    */
 	public static void blank() throws Exception {
         ObjectType type = ObjectType.get(getControllerClass());
         notFoundIfNull(type);
