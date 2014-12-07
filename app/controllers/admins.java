bd6feeefbc1a1233d0a75ae249b2eabb4c53491e
Brian <Brianmr31@gmail.com>
2014-12-07 23:43:52 +0700

    brian:resep add hampir selesai

--- app/controllers/admins.java
+++ app/controllers/admins.java
@@ -3,11 +3,46 @@ package controllers;
 import java.util.List;
 
 import models.bahan;
+import models.realresep;
+import models.resep;
 import play.mvc.Controller;
 
 public class admins extends Controller {
 	public static void lihatBahan(){
 		List m = bahan.findAll();
 		render(m);
+	}
+	public static void tambahResep(){
+		render();
+	}
+	public static void saveRealResep(realresep a){
+		a.save();
+		lihatResep();
+	}
+	public static void hapusResep(long id){
+		//resep.delete("realresep.id=?", id);
+		realresep.delete("id=?",id);
+	}
+	public static void lihatResep(){
+		List m = realresep.findAll();
+		render(m);
+	}
+	public static void lihatBahanR(long id){
+		realresep a;
+		a = realresep.find("id=?", id).first();
+		List m = resep.find("Nama_RealResep=?", a).fetch();
+		render(m);
+	}
+	public static void hapusBahanR(long id){
+		resep.delete("id=?", id);
+	}
+	public static void tambahBahanR(long a){
+		List m = resep.findAll() ;
+		List b = bahan.findAll();
+		render(m,b,a);
+	}
+	public static void saveBahanR(resep m){
+		m.save();
+		
 	}
 }
--- app/models/resep.java
+++ app/models/resep.java
@@ -6,16 +6,12 @@ import javax.persistence.ManyToOne;
 import play.db.jpa.Model;
 @Entity
 public class resep extends Model {
-	public String Nama_Resep;
 	@ManyToOne
 	public bahan Bahan;
 	public long Jumlah;
 	@ManyToOne
 	public satuan Satuan;
 	public long Harga ;
-	public String toString(){
-		return this.Nama_Resep;
-	}
 	@ManyToOne
 	public realresep Nama_RealResep;
 }
