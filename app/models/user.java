a9932c7318b85d52c24bc1f732a286eb3c3cca18
Mohandas Nur Alfan <alfanndaz@gmail.com>
2014-12-09 12:35:29 +0700

    alfan: secure layout + controler user+ model

--- app/models/user.java
+++ app/models/user.java
@@ -1,10 +1,18 @@
 package models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import play.db.jpa.Model;
 @Entity
 public class user extends Model{
 	public String user;
 	public String pass;
+	@ManyToOne
+	public HakAkses level;
+	
+	public String toString(){
+		return this.user;
+	}
 }
